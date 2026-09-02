22e4f217d98adfdde66b30eaa7d31eb85fad6d61
Mersad-Mostofian <mostofian.mersad@gmail.com>
2026-09-02 22:33:09 +0330

    feat: added sequence generator

---
+++ ml/src/inference/sequence_generator.py
@@ -0,0 +1,35 @@
+import torch
+
+def generate_sequnce(
+        model,
+        idx,
+        max_new_tokens,
+        context_size,
+        end_token_id=None
+):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+        if end_token_id is not None:
+            if torch.all(idx_next == end_token_id):
+                break
+    return idx
+
+def generate_protein(model, prompt, tokenizer,
+                      max_new_tokens, context_size, device, end_token="<|endofprotein|>"):
+    token_ids = tokenizer.encode(prompt, allowed_special={end_token})
+    idx = torch.tensor(
+        token_ids,
+        dtype=torch.long,
+        device=device,
+    ).unsqueeze(0)
+
+    end_token_id = tokenizer.inverse_vocab[end_token]
+    idx = generate_sequnce(model, idx, max_new_tokens, context_size, end_token_id=end_token_id)
+
+    return tokenizer.decode(idx[0].tolist())
