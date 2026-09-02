d311e49511e5460bf9cbe49f567a5cf67a17d321
Mersad-Mostofian <mostofian.mersad@gmail.com>
2026-09-02 23:02:26 +0330

    refactor: added topk and temperature optimization

--- ml/src/inference/sequence_generator.py
+++ ml/src/inference/sequence_generator.py
@@ -5,15 +5,29 @@ def generate_sequnce(
         idx,
         max_new_tokens,
         context_size,
-        end_token_id=None
+        end_token_id=None,
+        temperature=0,
+        topk=None
 ):
     for _ in range(max_new_tokens):
         idx_cond = idx[:, -context_size:]
         with torch.no_grad():
             logits = model(idx_cond)
         logits = logits[:, -1, :]
-        probas = torch.softmax(logits, dim=-1)
-        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+
+        if topk is not None:
+            top_logits, _ = torch,topk(logits, topk)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device), logits)
+
+        if temperature > 0:
+            logits = logits / temperature
+            logits = logits - logits.max(dim=-1, keepdim=True).values
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, 1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
         idx = torch.cat((idx, idx_next), dim=1)
         if end_token_id is not None:
             if torch.all(idx_next == end_token_id):
@@ -21,7 +35,9 @@ def generate_sequnce(
     return idx
 
 def generate_protein(model, prompt, tokenizer,
-                      max_new_tokens, context_size, device, end_token="<|endofprotein|>"):
+                      max_new_tokens, context_size,
+                        device, end_token="<|endofprotein|>",
+                        temperature=0, topk=None):
     token_ids = tokenizer.encode(prompt, allowed_special={end_token})
     idx = torch.tensor(
         token_ids,
@@ -30,6 +46,8 @@ def generate_protein(model, prompt, tokenizer,
     ).unsqueeze(0)
 
     end_token_id = tokenizer.inverse_vocab[end_token]
-    idx = generate_sequnce(model, idx, max_new_tokens, context_size, end_token_id=end_token_id)
+    idx = generate_sequnce(model, idx, max_new_tokens,
+                            context_size, end_token_id=end_token_id,
+                            temperature=temperature, topk=topk)
 
     return tokenizer.decode(idx[0].tolist())
